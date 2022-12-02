2a47a0127087de586bbe3d0651a9b1f799689c1f
Abdulatifkhan <anvarkhanov96@gamil.com>
2022-12-03 00:27:44 +0500

    Project

---
+++ web_scraping.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+from csv import writer
+
+access_datetime = datetime.datetime.now()
+source_url = "https://kun.uz/uz/news/2022/11/30/parda-ortidagi-xususiylashtirish-yirik-aktivlar-qanday-qilib-ofshor-kompaniyalarga-otib-ketmoqda"
+
+result = requests.get(source_url)
+doc = BeautifulSoup(result.text, "html.parser")
+tags = doc.find_all("p")
+
+
+with open("vocabulary.csv", "w", encoding="utf-8", newline="") as f:
+    thewriter = writer(f)
+    header = ["source_url", "access_datetime", "content", "word"]
+    thewriter.writerow(header)
+
+    for tag in tags:
+        content = tag.text.strip().replace("‘", "'").replace(
+            "’", "'").replace("–", "-").replace("“", '"').replace("”", '"')
+        info = [source_url, access_datetime, content, [word for word in content.split().lower()]]
+        thewriter.writerow(info)
