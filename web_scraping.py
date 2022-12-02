219d326987ce9ede1f40541bf484dcf48cdbe1a5
Abdulatifkhan <anvarkhanov96@gamil.com>
2022-12-03 00:53:04 +0500

    process info

--- web_scraping.py
+++ web_scraping.py
@@ -1,23 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 import datetime
+import time
 from csv import writer
 
-access_datetime = datetime.datetime.now()
-source_url = "https://kun.uz/uz/news/2022/11/30/parda-ortidagi-xususiylashtirish-yirik-aktivlar-qanday-qilib-ofshor-kompaniyalarga-otib-ketmoqda"
 
-result = requests.get(source_url)
-doc = BeautifulSoup(result.text, "html.parser")
-tags = doc.find_all("p")
+def process():
+    access_datetime = datetime.datetime.now()
+    source_url = "https://kun.uz/uz/news/2022/11/30/parda-ortidagi-xususiylashtirish-yirik-aktivlar-qanday-qilib-ofshor-kompaniyalarga-otib-ketmoqda"
+
+    result = requests.get(source_url)
+    doc = BeautifulSoup(result.text, "html.parser")
+    tags = doc.find_all("p")
+
+    with open("vocabulary.csv", "w", encoding="utf-8", newline="") as f:
+        thewriter = writer(f)
+        header = ["source_url", "access_datetime", "content", "word"]
+        thewriter.writerow(header)
+
+        for tag in tags:
+            content = tag.text.strip().replace("‘", "'").replace(
+                "’", "'").replace("–", "-").replace("“", '"').replace("”", '"')
+            info = [source_url, access_datetime, content, content.split()]
+            thewriter.writerow(info)
 
 
-with open("vocabulary.csv", "w", encoding="utf-8", newline="") as f:
-    thewriter = writer(f)
-    header = ["source_url", "access_datetime", "content", "word"]
-    thewriter.writerow(header)
-
-    for tag in tags:
-        content = tag.text.strip().replace("‘", "'").replace(
-            "’", "'").replace("–", "-").replace("“", '"').replace("”", '"')
-        info = [source_url, access_datetime, content, content.split()]
-        thewriter.writerow(info)
+if __name__ == '__main__':
+    while True:
+        process()
+        time.sleep(86400)
